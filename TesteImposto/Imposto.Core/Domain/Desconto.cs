ab6c605bc37d5b9314bf499c2bcafcde2c420eb5
alancordeiro <alan1cordeiro@gmail.com>
2015-10-20 21:13:30 -0200

    Atualizando

---
+++ TesteImposto/Imposto.Core/Domain/Desconto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imposto.Core.Domain
+{
+    public class Desconto
+    {
+        public void CalcularDesconto(NotaFiscal NF)
+        {
+            foreach (NotaFiscalItem itemNF in NF.ItensDaNotaFiscal)
+            {
+                if (NF.EstadoDestino == "SP" || NF.EstadoDestino == "RJ" || NF.EstadoDestino == "ES" || NF.EstadoDestino == "MG")
+                {
+                    itemNF.Desconto = 0.1*itemNF.BaseIPI;
+                }
+            }
+        }
+    }
+}
